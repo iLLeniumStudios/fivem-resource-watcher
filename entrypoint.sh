d58e8ef8379c01612716a853acc4550ab1cb0d8f
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-02-06 10:17:08 +0000

    Add file change check logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,18 @@ echo "$3"
 echo "$4"
 
 printenv
+${GITHUB_SHA}
+
+if [ ${GITHUB_BASE_REF} ]; then
+    # Pull Request
+    git fetch origin ${GITHUB_BASE_REF} --depth=1
+    export DIFF=$( git diff --name-only origin/${GITHUB_BASE_REF} ${GITHUB_SHA} )
+    echo "Diff between origin/${GITHUB_BASE_REF} and ${GITHUB_SHA}"
+else
+    # Push
+    git fetch origin ${GITHUB_EVENT_BEFORE} --depth=1
+    export DIFF=$( git diff --name-only ${GITHUB_EVENT_BEFORE} ${GITHUB_SHA} )
+    echo "Diff between ${GITHUB_EVENT_BEFORE} and ${GITHUB_SHA}"
+fi
+
+echo "${DIFF}"
